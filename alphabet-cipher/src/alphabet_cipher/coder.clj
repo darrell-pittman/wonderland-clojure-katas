431d20d47301cefaa8f0a53ec49ba022addc7b36
Darrell Pittman <darrell@darrellpittman.ca>
2017-11-26 18:46:12 -0330

    Alphabet Cipher solution

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,49 @@
 (ns alphabet-cipher.coder)
 
+(defn- offset [l]
+  (- (int l) (int \a)))
+
+(defn- letter [idx]
+  (char (+ 97 idx)))
+
+(def alphabet (range (int \a) (+ (int \z) 1)))
+(def alphabet-size (count alphabet))
+
+(defn- encode-char [k c]
+  (let [coff (offset c)
+        koff (offset k)
+        idx (+ coff koff)]
+    (char (first (drop idx (cycle alphabet))))))
+
+(defn- decode-char [k c]
+  (let [koff (offset k)
+        coff (+ (- alphabet-size koff) (offset c))]
+    (char (first (drop coff (cycle alphabet))))))
+
+(defn- decipher-char [c m]
+  (let [moff (offset m)
+        koff (- (+ alphabet-size (offset c)) moff)]
+    (letter (first (drop koff (cycle (range alphabet-size)))))))
+
+(defn- repeating-word [s]
+  (let [parts (rest (reductions str "" s))
+        matcher (fn [part]
+                  (let [words (partition (count part) s)
+                        has-words (> (count words) 1)
+                        matches (apply = words)]
+                    (when (and has-words matches)
+                      part)))]
+    (first (filter #(not (nil? %))
+        (map matcher parts)))))
+          
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str (map encode-char (cycle keyword) message)))
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply str (map decode-char (cycle keyword) message)))
 
 (defn decipher [cipher message]
-  "decypherme")
+  (let [keyword (apply str (map decipher-char cipher message))]
+    (repeating-word keyword)))
 
